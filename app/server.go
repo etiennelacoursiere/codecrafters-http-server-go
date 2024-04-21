0558e737d0b02bd1cb4e2fdd12501370b3e275b2
Etienne Lacoursiere <lacoursiere.etienne@gmail.com>
2024-04-21 13:08:58 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 
 	"github.com/codecrafters-io/http-server-starter-go/http"
 )
 
+var dir string
+
 func main() {
+	flag.StringVar(&dir, "directory", "", "Directory")
+	flag.Parse()
+
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -49,6 +56,8 @@ func handle_connection(conn net.Conn) {
 		handle_echo(conn, request)
 	case strings.HasPrefix(request.Path, "/user-agent"):
 		handle_user_agent(conn, request)
+	case strings.HasPrefix(request.Path, "/files"):
+		handle_files(conn, request)
 	default:
 		response := http.NotFound()
 		conn.Write(response.Serialize())
@@ -89,6 +98,37 @@ func handle_user_agent(conn net.Conn, request *http.Request) {
 			"Content-Length": strconv.Itoa(len(request.Headers["User-Agent"])),
 		},
 		Body: request.Headers["User-Agent"],
+	}
+
+	conn.Write(response.Serialize())
+}
+
+func handle_files(conn net.Conn, request *http.Request) {
+	filename, found := strings.CutPrefix(request.Path, "/files/")
+
+	if !found {
+		fmt.Println("Failed to parse request")
+		os.Exit(1)
+	}
+
+	filepath := path.Join(dir, filename)
+	buffer, error := os.ReadFile(filepath)
+
+	if error != nil {
+		response := http.NotFound()
+		conn.Write(response.Serialize())
+		return
+	}
+
+	response := &http.Response{
+		HTTPVersion: "HTTP/1.1",
+		StatusCode:  200,
+		Status:      "OK",
+		Headers: map[string]string{
+			"Content-Type":   "application/octet-stream",
+			"Content-Length": strconv.Itoa(len(buffer)),
+		},
+		Body: string(buffer),
 	}
 
 	conn.Write(response.Serialize())
